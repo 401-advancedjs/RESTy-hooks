a71063539473bfdeb9373e1e8d45351113946547
Melissa Stock <melissastock44@gmail.com>
2019-07-01 17:17:34 -0700

    used hooks to implement resty

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactJSON from 'react-json-view';
 import superagent from'superagent';
+import { useState } from 'react';
 
 import Form from '../Form/Form.js';
 import Header from '../Header/Header.js';
@@ -8,49 +9,42 @@ import Footer from '../Footer/footer.js';
 import JSONBody from '../JSONBody/JSONBody.js';
 
 import './App.scss';
-export default class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      URL: '',
-      method: '',
-      result: {},
-    }
+
+export default function App(){
+  const [URL, setURL] = useState({URL:''});
+  const [method, setMethod] = useState({method:''});
+  const [result, setResult] = useState({});
+
+  const handleURL = URL => {
+    setURL({ URL });
   };
 
-  handleURL = URL => {
-    this.setState({ URL });
+  const handleMethod = method => {
+    setMethod({ method });
   };
 
-  handleMethod = method => {
-    this.setState({ method });
+  const handleSubmit = async () => {
+    const response = await superagent(method.method, URL.URL);
+    const API_result = response.body.results;
+    console.log(API_result);
+    setResult({ result: API_result });
   };
 
-  handleSubmit = async () => {
-    const METHOD = this.state.method;
-    const URL = this.state.URL;
-    const response = await superagent(METHOD, URL);
-    const result = response.body.results;
-    this.setState({ result });
-  };
-
-  render(){
-    return(
+  return(
       <>
       <div>
         <Header />
         <Form 
-          handleURL    = { this.handleURL } 
-          handleMethod = { this.handleMethod } 
-          handleSubmit = { this.handleSubmit }
+          handleURL    = { handleURL } 
+          handleMethod = { handleMethod } 
+          handleSubmit = { handleSubmit }
         />
         <JSONBody />
         <div id="reactJSON">
-          <ReactJSON src={ this.state.result } />
+          <ReactJSON src={ result } />
         </div>
         <Footer />
       </div>
       </>
     );
-  }
-};
+}
